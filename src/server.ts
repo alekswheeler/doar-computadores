b011d67cbcaff1596f9b20d56b6c8b9b384a7e28
alekswheeler <alexskntks@outlook.com>
2022-08-03 10:21:33 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,6 @@ import { AppError } from "./errors/AppError";
 
 const app = router;
 
-app.get("/", (req, res)=>{
-  return res.json({message: "Server is running!"});
-})
-
 router.listen(3000, ()=>{
   console.log("Server is running at http://localhost:3000");
 });
